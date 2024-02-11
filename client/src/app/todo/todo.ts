bcba59d1cda3dae98d9fe81a9b0e5ab70df1544e
alijawosti <alijawosti@gmail.com>
2024-02-11 13:47:01 -0600

    Removing the accidental commits tot he main branch

--- client/src/app/todo/todo.ts
+++
@@ -1,9 +0,0 @@
-export class Todo {
-    _id: string;
-    owner: string;
-    status: boolean;
-    body: string;
-    category: string;
-  }
-
-  export type TodoCategory = 'groceries' | 'homework' | 'software design' | 'video games';
